959946e9bbb04f6339584f237940d4b8c8ae999c
Jonas Vermassen <jonas42vermassen@gmail.com>
2025-05-02 14:14:12 +0200

    reactoring

--- components/layout/tabs/tab-pane.js
+++ components/layout/tabs/tab-pane.js
@@ -1,14 +1,20 @@
-import { importComponent } from "../../../js/templater.js";
+import { importComponent} from "../../../js/templater.js";
 
-importComponent("tab-pane", (el, innerHTML = null) => {
-	let tabName = el.dataset.tab
-	let selected = el.dataset.selected
-	let pane = el.querySelector("#tab-pane-id")
-	pane.setAttribute("id", `${tabName}-tab-pane`)
-	pane.setAttribute("aria-labelled-by", `${tabName}-tab-head`)
-	if (selected) {
-		pane.classList.add("show", "active")
-	}
-	pane.innerHTML = innerHTML
 
-}, "./components/layout/tabs/tab-pane.html")
+function init(){
+
+	importComponent("tab-pane", (el, innerHTML = null) => {
+		let tabName = el.dataset.tab
+		let selected = el.dataset.selected
+		let pane = el
+		pane.setAttribute("id", `${tabName}-tab-pane`)
+		pane.setAttribute("aria-labelled-by", `${tabName}-tab-head`)
+		if (selected) {
+			pane.classList.add("show", "active")
+		}
+		pane.innerHTML = innerHTML
+		
+	}, "./components/layout/tabs/tab-pane.html")
+}
+
+init()
--- js/templater.js
+++ js/templater.js
@@ -3,7 +3,7 @@
 export let componentsRoot = "./components/"
 export let componentsExtension = ".html"
 
-export function importComponent(component, postScript = null, htmlFile = null) {
+export function importComponent_old(component, postScript = null, htmlFile = null) {
 	customElements.define(component,
 		class extends HTMLElement {
 			constructor() {
@@ -38,3 +38,43 @@ export function importComponent(component, postScript = null, htmlFile = null) {
 		});
 }
 
+
+export function importComponent(component, postScript = null, htmlFile = null) {
+	customElements.define(component,
+		class extends HTMLElement {
+			constructor() {
+				super();
+				if (htmlFile === null) {
+					htmlFile = componentsRoot + component + componentsExtension
+				}
+				fetch(htmlFile)
+					.then(file => file.text())
+					.then(
+						html => {
+							const doc = document.createElement("html");
+							doc.innerHTML = html;
+							const node = doc.querySelector("body").firstElementChild.cloneNode(true);
+
+
+							node.classList.add(...this.classList)
+							for (let d in this.dataset) {
+								node.setAttribute(`data-${d}`, this.dataset[d])
+							}
+							this.getAttributeNames().forEach(name => {
+								if (!(name === "class") || name.startsWith("data-")) {
+									node.setAttribute(name, this.getAttribute(name))
+								}
+							});
+
+							if (postScript) {
+								postScript(node, this.innerHTML)
+							} else {
+								node.innerHTML = this.innerHTML
+							}
+							this.replaceWith(node)
+						}
+					);
+
+			}
+		});
+}
