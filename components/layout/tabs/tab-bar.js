df3b99e221f89787c3a4c6e45030218fe82ae796
Jonas Vermassen <jonas42vermassen@gmail.com>
2025-05-02 20:51:54 +0200

    set up with vite

--- components/layout/tabs/tab-bar.js
+++
@@ -1,3 +0,0 @@
-import { importComponent } from "../../../js/templater.js";
-
-importComponent("tab-bar", null, "./components/layout/tabs/tab-bar.html")
--- components/layout/tabs/tab-body.js
+++
@@ -1,3 +0,0 @@
-import { importComponent } from "../../../js/templater.js";
-
-importComponent("tab-body",null, "./components/layout/tabs/tab-body.html")
